34cc65894e057cd5bf995b43936ada52ed484b33
Antony <soporte@plaza.net.co>
2024-10-30 22:59:52 -0500

    Fomr - header-footer y body

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <div className="grid grid-cols-6 gap-4 items-center justify-center m-auto">
+                <div className="col-start-1 col-end-3 ...">
+                    valores
+                </div>
+                <div className="col-end-7 col-span-2 ...">
+                    valores
+                </div>
+                <div className="col-start-1 col-end-7 ...">valores</div>
+            </div>
+        </>
     )
 }
 
